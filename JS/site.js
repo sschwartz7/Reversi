d29b76fc9cd38f438a425f0a90bfd4f71573c930
Simon Schwartz <sschwartz794@gmail.com>
2023-07-25 13:19:52 -0500

    Reversed a String added code and added images

--- JS/site.js
+++ JS/site.js
@@ -1,12 +1,24 @@
-function displayMessage(){
-    let inputBox= document.getElementById('message');
-    let message= inputBox.value;
-
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: message
-        }
-      );
+//entry point
+function getValues(){
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
 }
+
+//logic
+function reverseString(userString){
+    let revString = '';
+     for (let index = userString.length-1; index >= 0; index--) {
+         revString+= userString[index];         
+     }
+     return(revString);
+}
+
+//display
+function displayString(revString){
+    document.getElementById('results').textContent = revString;
+    let alertBox = document.getElementById('alert');
+    alertBox.classList.remove('invisible');
+
+}
+
